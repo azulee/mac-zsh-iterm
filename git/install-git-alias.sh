00e8b1400d717a3a391c1b5a0d5cfdc83c7b0fbf
azulee <azuleemail@gmail.com>
2018-02-19 10:37:01 -0800

    updated sh script

--- git/install-git-alias.sh
+++ git/install-git-alias.sh
@@ -1,21 +1,27 @@
 #!/bin/sh
-echo "# GIT ALIASES" >> ~/.zshrc
-echo "alias gs='git status'" >> ~/.zshrc
-echo "alias gd='git diff'" >> ~/.zshrc
-echo "alias ga='git add'" >> ~/.zshrc
-echo "alias gaa='git add .'" >> ~/.zshrc
-echo "alias gcm='git commit -m'" >> ~/.zshrc
-echo "alias gl='git log'" >> ~/.zshrc
-echo "alias gblm='git blame'" >> ~/.zshrc
-echo "alias gb='git branch'" >> ~/.zshrc
-echo "alias gc='git checkout'" >> ~/.zshrc
-echo "alias gdb='git branch -d'" >> ~/.zshrc
-echo "alias gremote='git remote'" >> ~/.zshrc
-echo "alias gf='git fetch'" >> ~/.zshrc
-echo "alias gp='git pull'" >> ~/.zshrc
-echo "alias gpush='git push'" >> ~/.zshrc
-echo "alias gm='git merge'" >> ~/.zshrc
-echo "alias gmt='git mergetool'" >> ~/.zshrc
-echo "alias grebase='git rebase'" >> ~/.zshrc
-echo "alias ghr='git reset --hard HEAD'" >> ~/.zshrc
-echo "alias gsr='git reset --soft HEAD'" >> ~/.zshrc
+echo "\n# GIT ALIASES" >> ~/.zshrc
+declare -a myAliases=(
+	"gs='git status'"
+	"gd='git diff'"
+	"ga='git add'"
+	"gaa='git add .'"
+	"gcm='git commit -m'"
+	"gl='git log'"
+	"gblm='git blame'"
+	"gb='git branch'"
+	"gc='git checkout'"
+	"gdb='git branch -d'"
+	"gremote='git remote'"
+	"gf='git fetch'"
+	"gp='git pull'"
+	"gpush='git push'"
+	"gm='git merge'"
+	"gmt='git mergetool'"
+	"grebase='git rebase'"
+	"ghr='git reset --hard HEAD'"
+	"gsr='git reset --soft HEAD'"
+	)
+for ((i = 0; i < ${#myAliases[@]}; i++))
+do
+	echo "alias ${myAliases[$i]}" >> ~/.zshrc
+done
